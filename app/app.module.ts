8daefc5fcf40d0f81f26237a2213d408d72e4cf6
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2016-11-26 20:08:09 -0200

    7. HTTP - Step 1 (In memory data service)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -9,18 +14,13 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component'
 import { HeroService } from './hero.service';
 
-import { AppRoutingModule } from './app-routing.module';
-
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-      { path: 'heroes', component: HeroesComponent },
-      { path: 'detail/:id', component: HeroDetailComponent },
-      { path: 'dashboard', component: DashboardComponent }
-    ])
+    HttpModule,
+    AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
   ],
   declarations: [
     AppComponent,
@@ -28,7 +28,7 @@ import { AppRoutingModule } from './app-routing.module';
     HeroDetailComponent,
     HeroesComponent
   ],
-  providers: [ HeroService ],
-  bootstrap: [ AppComponent ]
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
