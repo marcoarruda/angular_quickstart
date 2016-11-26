0ffd2ac9b9c597c976cfc53a6664af540674ad75
Marco Antonio Arruda <marco.nc.arruda@gmail.com>
2016-11-26 18:39:23 -0200

    6. Routing - Step 3 (Dashboard component with routing)

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component'
 import { HeroService } from './hero.service';
@@ -15,11 +16,14 @@ import { HeroService } from './hero.service';
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: 'heroes', component: HeroesComponent }
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      { path: 'heroes', component: HeroesComponent },
+      { path: 'dashboard', component: DashboardComponent }
     ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],
